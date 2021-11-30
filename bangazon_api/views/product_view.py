55b575fba546d7f070febe231ac968e4b4482f09
Blake Rhoden <zbrhoden@gmail.com>
2021-11-30 13:03:18 -0600

    bug 4

--- bangazon_api/views/product_view.py
+++ bangazon_api/views/product_view.py
@@ -169,7 +169,7 @@ class ProductView(ViewSet):
         if number_sold:
             products = products.annotate(
                 order_count=Count('orders')
-            ).filter(order_count__lt=number_sold)
+            ).filter(order_count__gt=number_sold)
 
         if order is not None:
             order_filter = f'-{order}' if direction == 'desc' else order
@@ -247,6 +247,7 @@ class ProductView(ViewSet):
             product = Product.objects.get(pk=pk)
             order = Order.objects.get(
                 user=request.auth.user, completed_on=None)
+            product.delete()
             return Response(None, status=status.HTTP_204_NO_CONTENT)
         except Product.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
